ee4e7abaa32b7501e26b8927f0ad161a7765aa2b
Marc de Mas <marcdmv@gmail.com>
2020-09-09 14:31:44 +0200

    Subida Fase 2

---
+++ Fase2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Character> nom = new ArrayList<>();
+        nom.add('m');
+        nom.add('a');
+        nom.add('r');
+        nom.add('c');
+        for (char i : nom){
+            if (i=='a' || i=='e' || i=='i' || i=='o' || i=='u'){
+                System.out.println("VOCAL");
+            } else if (Character.isDigit(i)) {
+                System.out.println("Els noms de persones no contenen números!’.");
+            } else {
+                System.out.println("CONSONANT");
+            }
+        }
+    }
+}
