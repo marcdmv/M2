3317bfc87ac6eb92c84f8002024e3ba9f2ecfe65
Marc de Mas <marcdmv@gmail.com>
2020-09-09 14:35:25 +0200

    Añado apellido para que haya más letras

--- Fase1.java
+++ Fase1.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        char[] nom = new char[]{'m','a','r','c'};
+        char[] nom = new char[]{'m','a','r','c','d','e','m','a','s'};
 
         for (char i : nom){
             System.out.println(i);
