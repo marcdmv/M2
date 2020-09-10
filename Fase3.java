6bc3ebbd7b1ac4f5f4e056642ecdea33e8efadce
Marc de Mas <marcdmv@gmail.com>
2020-09-10 09:58:03 +0200

    Quito lista ya que no se usa

--- Fase3.java
+++ Fase3.java
@@ -3,19 +3,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        List<Character> nom = new ArrayList<>();
-        nom.add('m');
-        nom.add('a');
-        nom.add('r');
-        nom.add('c');
-        nom.add('d');
-        nom.add('e');
-        nom.add('m');
-        nom.add('a');
-        nom.add('s');
-
-        List<Integer> repetitions = new ArrayList<>();
-        int repetitionsPos = 0;
         Hashtable<Character, Integer> contenedor = new Hashtable<Character, Integer>();
 
             contenedor.put('m',2);
